f1b0bf01c53e76b162defb79070a4d5f6e93d74d
Akshay chaudhary <akshaypoonia00000@gmail.com>
2026-06-16 00:01:28 +0530

    FAST api learnings first commit

---
+++ main_01.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
+    age: int
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {"message": "Hello from venv"}
+
+@app.get('/about')
+def about():
+    return {"Message": "My name is Akshay Chaudhary, I'm a software engineer"}
+
+
+@app.get('/users')
+def users():
+    users = ["Akshay", "Vishal", "kahshdf"]
+
+    return {
+        "users": users
+    }
+
+@app.get('/user/{user_id}')
+def get_user(user_id: int):
+    return {"user": user_id}
+
+@app.get('/params')
+def params(name: str = None):
+    return {"name": name}
